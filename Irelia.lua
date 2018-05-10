bc2c26e479878daaf18b8b1cb89ef51b7d815a1c
bunnyriven <38993176+bunnyriven@users.noreply.github.com>
2018-05-10 05:48:18 +0200

    Update Irelia.lua

--- Irelia.lua
+++ Irelia.lua
@@ -1,9 +1,5 @@
 class "Irelia"
 
-require = 'DamageLib'
-require = 'Collision'
-require = 'Tpred'
-
 
 function Irelia:LoadSpells()
 
@@ -12,94 +8,6 @@ function Irelia:LoadSpells()
 	E = {Range = 325, Width = 70, Delay = 0.25, Speed = 1200, Collision = false, aoe = false, Type = "line"}
 	R = {Range = 1000, Width = 0, Delay = 0.25, Speed = 1200, Collision = false, aoe = false, Type = "line"}
 
-end
-
-function Irelia:LoadMenu()
-
---Main Menu
-
-self.Menu = MenuElement({type = MENU, id = "Irelia", name = "BunnyIrelia"})
-
---Combo Settings Menu
-
-	self.Menu:MenuElement({type = MENU, id = "Irelia", name = "BunnyIrelia"})
-	self.Menu:MenuElement({id = "Combo", name = "Combo", type = MENU})
-	self.Menu.Combo:MenuElement({id = "UseQ", name = "Q", value = true})
-	self.Menu.Combo:MenuElement({id = "UseW", name = "W", value = true})
-	self.Menu.Combo:MenuElement({id = "UseE", name = "E", value = true})
-	self.Menu.Combo:MenuElement({id = "EStun", name = "Only E to Stun?", value = true})
-	self.Menu.Combo:MenuElement({id = "RKey", name = "R Key", key = string.byte("T")})
-	self.Menu.Combo:MenuElement({id = "comboActive", name = "Combo key", key = string.byte(" ")})	
-	
-  
-  --Harass Settings Menu
-  
-	self.Menu:MenuElement({id = "Harass", name = "Harass", type = MENU})
-	self.Menu.Harass:MenuElement({id = "UseQ", name = "Q", value = true})
-	self.Menu.Harass:MenuElement({id = "UseW", name = "W", value = true})
-	self.Menu.Harass:MenuElement({id = "UseE", name = "E", value = true})
-	self.Menu.Harass:MenuElement({id = "harassActive", name = "Harass key", key = string.byte("V")})
-
-
---Lasthit Settings Menu
-
-	self.Menu:MenuElement({id = "Lasthit", name = "Lasthit", type = MENU})
-	self.Menu.Lasthit:MenuElement({id = "UseQ", name = "Q", value = true})
-	self.Menu.Lasthit:MenuElement({id = "lasthitActive", name = "Lasthit key", key = string.byte("X")})
-  
-  
-	--Gapclose Settings Menu
-  
-	self.Menu:MenuElement({id = "Gapclose", name = "Gapclose", type = MENU})
-	self.Menu.Gapclose:MenuElement({id = "QCombo", name = "Q on Combo", value = true})
-	self.Menu.Gapclose:MenuElement({id = "QKS", name = "Q > Minion > KS Target", value = true})
-  
-  
-	--KS
-  
-	self.Menu:MenuElement({id = "Killsteal", name = "Killsteal", type = MENU})
-	self.Menu.Killsteal:MenuElement({id = "KS", name = "Enemies to use:", type = MENU})
-	for i, hero in pairs(GetEnemyHeroes()) do
-	self.Menu.Killsteal.KS:MenuElement({id = "KS"..hero.charName, name = ""..hero.charName, value = true})
-	end
-	self.Menu.Killsteal:MenuElement({id = "UseQ", name = "Q", value = true})
-	self.Menu.Killsteal:MenuElement({id = "UseE", name = "E", value = true})
-	self.Menu.Killsteal:MenuElement({id = "UseR", name = "Use R? Calculates all 4!", value = true})
-  
-  
-  --Drawings Settings Menu
-	
-	self.Menu:MenuElement({id = "Drawings", name = "Drawings", type = MENU})
-	--Q
-	self.Menu.Drawings:MenuElement({id = "Q", name = "Draw Q range", type = MENU})
-  self.Menu.Drawings.Q:MenuElement({id = "Enabled", name = "Enabled", value = true})       
-  self.Menu.Drawings.Q:MenuElement({id = "Width", name = "Width", value = 1, min = 1, max = 5, step = 1})
-  self.Menu.Drawings.Q:MenuElement({id = "Color", name = "Color", color = Draw.Color(200, 255, 255, 255)})	
-	--W
-	-- self.Menu.Drawings:MenuElement({id = "W", name = "Draw W range", type = MENU})
-    -- self.Menu.Drawings.W:MenuElement({id = "Enabled", name = "Enabled", value = true})       
-    -- self.Menu.Drawings.W:MenuElement({id = "Width", name = "Width", value = 1, min = 1, max = 5, step = 1})
-    -- self.Menu.Drawings.W:MenuElement({id = "Color", name = "Color", color = Draw.Color(200, 255, 255, 255)})	
-	--E
-	self.Menu.Drawings:MenuElement({id = "E", name = "Draw E range", type = MENU})
-    self.Menu.Drawings.E:MenuElement({id = "Enabled", name = "Enabled", value = false})       
-    self.Menu.Drawings.E:MenuElement({id = "Width", name = "Width", value = 1, min = 1, max = 5, step = 1})
-    self.Menu.Drawings.E:MenuElement({id = "Color", name = "Color", color = Draw.Color(200, 255, 255, 255)})	
-	--R
-	self.Menu.Drawings:MenuElement({id = "R", name = "Draw R range", type = MENU})
-    self.Menu.Drawings.R:MenuElement({id = "Enabled", name = "Enabled", value = true})       
-    self.Menu.Drawings.R:MenuElement({id = "Width", name = "Width", value = 1, min = 1, max = 5, step = 1})
-    self.Menu.Drawings.R:MenuElement({id = "Color", name = "Color", color = Draw.Color(200, 255, 255, 255)})
-	
-	self.Menu.Drawings:MenuElement({id = "DrawDamage", name = "Draw damage on HPbar", value = true})
-   self.Menu.Drawings:MenuElement({id = "HPColor", name = "HP Color", color = Draw.Color(200, 255, 255, 255)})
-
-	self.Menu:MenuElement({id = "CustomSpellCast", name = "Use custom spellcast", tooltip = "Can fix some casting problems with wrong directions and so", value = true})
-	self.Menu:MenuElement({id = "delay", name = "Custom spellcast delay", value = 50, min = 0, max = 200, step = 5,tooltip = "increase this one if spells is going completely wrong direction", identifier = ""})
-	
-	self.Menu:MenuElement({id = "blank", type = SPACE , name = ""})
-	self.Menu:MenuElement({id = "blank", type = SPACE , name = "Script Ver: "..Version.. " - LoL Ver: "..LVersion.. ""})
-	self.Menu:MenuElement({id = "blank", type = SPACE , name = "by "..Author.. ""})
 end
 
 
